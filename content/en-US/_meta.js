fee2258841302af78ac536fe288d24e63ebd8c2c
MEFRREEX <mefrreex@gmail.com>
2026-07-10 12:50:31 +0200

    feat: installation guide

--- content/en-US/_meta.js
+++ content/en-US/_meta.js
@@ -1,5 +1,6 @@
 export default {
   index: 'Home',
+  installation: 'Installation',
   docs: 'Documentation',
   versioning: 'Versioning'
 }
--- content/ru-RU/_meta.js
+++ content/ru-RU/_meta.js
@@ -1,5 +1,6 @@
 export default {
   index: 'Главная',
+  installation: 'Установка',
   docs: 'Документация',
   versioning: 'Версионирование'
 }
